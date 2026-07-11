720a69e4ccf99dd1f075789b4b813dc1efce6cd9
chroksys <christianokitovs@gmail.com>
2026-07-11 12:41:17 +0300

    Fix blank screen on older mobile devices by adding Vite legacy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   plugins: [
     react(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    }),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['logo(Dark).svg', 'logo(Light).svg'],
