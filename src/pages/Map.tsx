52242d5dafd0a1925c36285ee23578c5eb162935
chroksys <christianokitovs@gmail.com>
2026-07-13 14:39:35 +0300

    Fix Map component collapsing by adding 100% width and height

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -134,6 +134,7 @@ export const MapView: React.FC = () => {
   return (
     <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: '#000', zIndex: 10 }}>
       <Map
+        style={{ width: '100%', height: '100%' }}
         initialViewState={{
           longitude: 32.5825,
           latitude: 0.3476,
